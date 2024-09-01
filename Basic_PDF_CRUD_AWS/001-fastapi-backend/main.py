1e19c5c100a8c3834d49b5e89d03734985f9bcef
Richard M. Ñope-Giraldo <81273091+Richard97chz@users.noreply.github.com>
2024-08-31 22:07:41 -0500

    Update main.py

--- Basic_PDF_CRUD_AWS/001-fastapi-backend/main.py
+++ Basic_PDF_CRUD_AWS/001-fastapi-backend/main.py
@@ -19,7 +19,7 @@ app.include_router(todos.router)
 
 origins = [
     "http://localhost:3000",
-    "https://todo-frontend-khaki.vercel.app/",
+    "https://basic-pdf-crud-aws-5.onrender.com",
 ]
 
 # CORS configuration, needed for frontend development
